e7f4e42fa314e86e6a56caf9cb5ee6bc14ab006e
Aditya Gupta <aditya6403t@gmail.com>
2025-07-29 23:35:27 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://imagify-frontend-u9m6.onrender.com"], // Your deployed frontend
+  credentials: true
+}));
+
 
 await connectDB(); // ✅ Top-level await is allowed in ES Modules
 app.use('/api/user', userRouter)
